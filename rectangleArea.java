7dce5ff9fe40672b4f5428e530f7cd05587268e7
Ekariorama Magaleta <bsc-com-07-19@unima.ac.mw>
2021-12-14 16:56:38 -0800

    added license file

---
+++ rectangleArea.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class rectangleArea{
+  public static void main(String [] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("The area will be shown here");
+    /*System.out.println("Enter length: ");
+      double length = input.nextDouble();
+    System.out.println("Enter breadth: "); 
+      double breadth = input.nextDouble();
+      Area area1 = new Area(length,breadth);
+    System.out.println("The area is " + area1.returnArea());*/
+ 
+  }
+}
+/*class Area{
+  private double length,breadth,area;
+  Area(double L, double B){
+    length = L;
+    breadth = B;
+}
+  public double returnArea(){
+    area = length*breadth;
+    return area;
+}
+}*/
